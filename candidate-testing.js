9e30cbd89bce4717b0b01db5920de37d13bad8f5
lukicusi <lukicusi@gmail.com>
2024-06-28 07:40:38 -0500

    candidate-testing pt2 completed, 10 passing

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,22 @@ let candidateAnswer = " ";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ", 
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? ",
+  // lucas- these should be arrays to store user response? loop over quesstion to go voer all questions
+]
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3",
+]
+let candidateAnswers = []
 
 
 function askForName() {
@@ -23,19 +36,26 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
-
+  //candidateAnswer = input.question(questions[0]);
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswer = input.question(questions[i]);
+    candidateAnswers.push(candidateAnswer);
+  }
+  return candidateAnswer;
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === "Sally Ride"){
+console.log(`Your answers were: ${candidateAnswers} \n The correct answers were: ${correctAnswers}`)
+
+
+/*  if ((candidateAnswers) === (correctAnswers)) {
   console.log("this is correct");
 } else {
   console.log("this is incorrect");
 }
-
+*/
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
